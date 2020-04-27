fc53ea86db49c5feb3e64d7ee144451ea0c4b49b
rdiego26 <rdieg26@gmail.com>
2020-04-27 18:50:29 -0300

    WIP on initial view

---
+++ app/src/main/java/me/diegoramos/aluratravel/ui/activity/PackageListActivity.kt
@@ -0,0 +1,13 @@
+package me.diegoramos.aluratravel.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import me.diegoramos.aluratravel.R
+
+class PackageListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_package_list)
+    }
+}
