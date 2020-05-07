910caea883a78c5f4cf0747831f41f9e19589464
rdiego26 <rdieg26@gmail.com>
2020-05-07 08:29:11 -0300

    WIP on payment view

---
+++ app/src/main/java/me/diegoramos/aluratravel/ui/activity/PaymentActivity.kt
@@ -0,0 +1,16 @@
+package me.diegoramos.aluratravel.ui.activity
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import me.diegoramos.aluratravel.R
+
+class PaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+
+
+    }
+
+}
